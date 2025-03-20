8fb4371a943c1db9cde0eff4da93b02a4ba06b7c
Hans Steffens <hans@steffens.co>
2025-03-20 18:18:45 -0500

    Create a helper function that determines the age, which both display_age() and upcoming_birthdays() can use.

--- functions.py
+++ functions.py
@@ -1,6 +1,16 @@
 from dateutil import relativedelta
 import datetime
 
+# This function is used to calculate the age of a person
+# It takes a string as input, which is the birthdate of the person
+# It returns the age of the person
+def calculate_age(birthdate):
+    format_string = "%Y-%m-%d"
+    birthday_dt = datetime.datetime.strptime(birthdate, format_string)
+    today = datetime.datetime.today()
+    age = relativedelta.relativedelta(today, birthday_dt) # returns a relativedelta object e.g. relativedelta(years=0, months=0, days=0)
+    return age
+
 def upcoming_birthdays(people_list, days):
     # TODO: write code that finds all upcoming birthdays in the next 90 days
     # 90 is passed in as a parameter from menus.py
@@ -12,31 +22,31 @@ def upcoming_birthdays(people_list, days):
     #pass
     for person in people_list:
         format_string = "%Y-%m-%d"
+        # birthday_dt represents the birthday of the person
         birthday_dt = datetime.datetime.strptime(person['birthday'], format_string)
         now = datetime.datetime.now()
 
         birthday_this_year =birthday_dt.replace(year=now.year)
         difference = birthday_this_year - now
 
-        turning_age = relativedelta.relativedelta(now, birthday_dt).years + 1
+        age_now = calculate_age(person['birthday'])
+        turning_age = age_now.years + 1
 
         if 0 < difference.days < days:
-            print(f"{person['name']} turns {turning_age}  in {difference.days} days on {birthday_dt.strftime('%B %d')}")
+            print(f"{person['name']} turns {turning_age} in {difference.days} days on {birthday_dt.strftime('%B %d')}")
 
 def display_age(person):
-    # TODO: write code to display the age of person
+    # TODO: write the code to display the age of a person
     # Template:
     # PERSON is X years, X months, and X days old
-    format_string = "%Y-%m-%d"
-    birthday_dt = datetime.datetime.strptime(person['birthday'], format_string)
-    today = datetime.date.today()
-
-    difference = relativedelta.relativedelta(today, birthday_dt)
-    #print(difference)
-
-    print(f"{person['name']} is {difference.years} years, {difference.months} months, and {difference.days} days old")
-    #print(person)
-    #pass
+    #print("Display Age function")
+    
+    #  age variable is a relativedelta object and contains years, months, days. It uses the calculate_age function.
+    age = calculate_age(person['birthday'])
+    #print age variable
+    #print(age)
+    # print the age of the person using the format string, e.g. "John is 25 years, 3 months, and 2 days old"
+    print(f"{person['name']} is {age.years} years, {age.months} months, and {age.days} days old")
 
 
 def display_age_difference(people):
